5dfb7de79e5880adccb672d31498da8b5c7d6710
Minh Truong <minh@truong.fi>
2022-01-21 10:24:45 +0200

    Add FindvalueInTemplate script

---
+++ findValueInTemplate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Find value in running CloudFormation template in AWS account
+#
+# This require aws-cli and jq to be installed
+
+# Set here AWS Profile ~/.aws/profile if it's not default
+PROFILE=default
+
+## Don't edit below
+
+FIND=$1
+
+if [[ -z "${FIND}" ]]; then
+    echo "Syntax: $0 <StringToFind>"
+    exit 1
+fi
+
+# Get all all Cloudformation stacks from AWS account
+STACKS=$(aws --profile ${PROFILE} cloudformation list-stacks)
+STACKID=$(echo $STACK | jq -r '.StackSummaries[].StackId')
+
+# Loop all stacks
+for STACK in $STACKID
+do
+    TEMPLATE=$(aws --profile ${PROFILE} cloudformation get-template --stack-name ${STACK} | jq -c '.TemplateBody' | grep "${FIND}")
+    # If keyword found in stack
+    if [[ ! -z "${TEMPLATE}" ]]; then
+        echo "Found result in Stack: $STACK"
+    fi
+done
